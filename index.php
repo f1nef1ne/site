e475b14d4b0cac52ae5eb930e699fb743ffb239b
Vlad Reminny <recatdesign@gmail.com>
2015-01-22 15:41:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="recat.css">
+	<title></title>
+</head>
+<body>
+
+<input type = 'checkbox' id  = 'show-menu' class = 'show-menu'>
+<label class = 'hamburger' tabindex="1" for = "show-menu">
+	<div></div>
+	<div></div>
+	<div></div>
+</label>
+<div class = 'menu-container'>
+	<ul>
+		<li>Start</li>
+		<li>Articles</li>
+		<li>Contats</li>
+	</ul>
+</div>
+<h1>Recat</h1>
+<p>Testing Git</p>
+<label for = "show-menu" class = 'fill'></label>
+
+</body>
+</html>
